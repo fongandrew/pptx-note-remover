31a0c765ddd17b6cce94ef754006eef7383fd62b
fongandrew <lx2798@gmail.com>
2014-05-31 19:32:35 -0700

    Cross-platform setup script
    Not entirely sure this works -- I don't have my OS X system on me -- so
    this may need some tweaking

--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
-from distutils.core import setup
-import py2exe
+"""
+py2app/py2exe build script for PPTX removal tool.
 
-setup(options = {'py2exe': {'bundle_files': 1}},
-      console = ['drop_pptx_here.py'],
-      zipfile = None)
+Usage (Mac OS X):
+ python setup.py py2app
+
+Usage (Windows):
+ python setup.py py2exe
+
+"""
+import sys
+from distutils.core import setup
+
+mainscript = 'drop_pptx_here.py'
+
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
+    )
+elif sys.platform == 'win32':
+    extra_options = dict(
+        setup_requires=['py2exe'],
+        app = [mainscript],
+        options = dict(py2exe=dict(bundle_files=1, argv_emulation=True)),
+    )
+else:
+    extra_options = dict(
+        # Normally unix-like platforms will use "setup.py install"
+        # and install the main script as such
+        scripts=[mainscript],
+    )
+
+setup(
+    name="drop_pptx_here",
+    **extra_options
+)
