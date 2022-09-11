cdf0cbce8499da4bb4bb1570fe4369ab673094a4
Andrew Fong <afong@slack-corp.com>
2022-09-11 14:39:52 -0700

    Update to python3

--- drop_pptx_here.py
+++ drop_pptx_here.py
@@ -6,22 +6,22 @@ def rm_txt(str):
     return re.sub(r'<p:txBody>.*</p:txBody>', '<p:txBody><a:bodyPr/><a:lstStyle/><a:p><a:endParaRPr/></a:p></p:txBody>', str)
 
 def main(fn):
-    print "Processing %s -- please wait." % fn
+    print("Processing %s -- please wait." % fn)
     if fn[-5:] != '.pptx':
         raise RuntimeError("Files need to be .pptx files.")
     old = zipfile.ZipFile(fn, "r")
     fn2 = fn.replace(".pptx","-clean.pptx")
     new = zipfile.ZipFile(fn2, "w")
     for item in old.infolist():
-        data = old.read(item.filename)
+        data = old.read(item.filename).decode('utf-8')
         if item.filename.startswith("ppt/notesSlides/notesSlide") \
                 and item.filename.endswith(".xml"):
-            print ". . .", "cleaning", item.filename
+            print(". . .", "cleaning", item.filename)
             data = rm_txt(data)
         new.writestr(item, data)
     new.close()
     old.close()
-    print "Complete. Saved as", fn2
+    print("Complete. Saved as", fn2)
 
 if __name__ == '__main__':
     try:
@@ -29,10 +29,9 @@ if __name__ == '__main__':
             for arg in sys.argv[1:]:
                 fn = os.path.abspath(arg)
                 main(fn)
-                print "---"
+                print("---")
         else:
-            print "You need to drag your .pptx file(s) onto this one."
+            print("You need to drag your .pptx file(s) onto this one.")
     finally:
-        raw_input("Press any key to quit.")
-        
-        
+        input("Press any key to quit.")
+
